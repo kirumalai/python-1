675c2d89b9ade7c12b05b4f3fd63ecfe26b34142
NandhiniManohar22 <51949323+NandhiniManohar22@users.noreply.github.com>
2019-06-27 15:03:11 +0530

    Update 10.py

--- 10.py
+++ 10.py
@@ -1,6 +1,6 @@
-n1,n2=map(int,input().split())
-n3=list(map(int,input().split()))
+x1,x2=map(int,input().split())
+x3=list(map(int,input().split()))
 sum=0
-for i in range(n2):
-    sum=sum+n2[i]
+for i in range(x2):
+    sum=sum+x2[i]
 print(sum)    
