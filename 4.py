1ff08c5e1e31c169bfad3698a17315abff48786b
NandhiniManohar22 <51949323+NandhiniManohar22@users.noreply.github.com>
2019-06-27 13:31:15 +0530

    Update 4.py

--- 4.py
+++ 4.py
@@ -3,7 +3,7 @@ A=('a','e','i','o','u')
 B=('b','c','d','e','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z')
 if l in A:
    print("Vowel")
-elif l B:
+elif l in B:
    print("Consonant")
 else:
    print("invalid")
